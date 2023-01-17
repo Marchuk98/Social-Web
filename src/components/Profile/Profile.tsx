406fb48dd6810ebc38eb804af3a8786ab92162d1
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-17 21:02:31 +0300

    Code improvement

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,27 +1,13 @@
-import s from './Profile.module.css'
+import React from "react";
+import MyPost from "./MyPosts/MyPost";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
 
 const Profile = () => {
     return (
-
-        <div className={s.content}>
-
-            <div>
-                <img
-                    src="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/b0b69b110218509.5fe75aca3a23e.jpg"
-                    alt=""/>
-            </div>
-
-            <div>ava</div>
-
-            <div>my post
-
-                <div>New post</div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2
-                    </div>
-                </div>
-            </div>
+        <div>
+            <ProfileInfo/>
+            <MyPost/>
         </div>
 
     );
