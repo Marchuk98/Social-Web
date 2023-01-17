4458f95b81b413c981e56f9f50691cc5457d9ee5
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-17 21:03:20 +0300

    Rendering the ProfileInfo components

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from './ProfileInfo.module.css'
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img
+                    src="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/b0b69b110218509.5fe75aca3a23e.jpg"
+                    alt=""/>
+            </div>
+            <div className={s.DescriptionBlock}>ava + des</div>
+        </div>
+
+    );
+}
+
+
+export default ProfileInfo;
