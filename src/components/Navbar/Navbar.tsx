4b35182b34742fd85fe60c2de99389c716e4a48a
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-17 20:51:24 +0300

    Switching to NavLink with route indication

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,25 @@
 import s from './Navbar.module.css'
+import { NavLink, useLocation  } from "react-router-dom";
 
 const Navbar = () => {
+    const location = useLocation();
+    const path = location.pathname;
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='#'>Profile</a>
+                <NavLink to='/profile' className={`${s.item} ${path === '/profile' ? s.activeLink : ''}`}>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/dialogs" className={`${s.item} ${path === '/dialogs' ? s.activeLink : ''}`}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href='#'>Messages</a>
+                <NavLink to="/news" className={`${s.item} ${path === '/news' ? s.activeLink : ''}`}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href='#'>News</a>
+                <NavLink to="/music" className={`${s.item} ${path === '/music' ? s.activeLink : ''}`}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href='#'>Music</a>
-            </div>
-            <div className={s.item}>
-                <a href='#'>Settings</a>
+                <NavLink to="/settings" className={`${s.item} ${path === '/settings' ? s.activeLink : ''}`}>Settings</NavLink>
             </div>
         </nav>
 
