7267ff85886611bbd3a775a96d5d75994892b24a
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-24 21:18:42 +0300

    Typing with data addition via props

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import MyPost from "./MyPosts/MyPost";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {ProfilePageType} from "../../redux/state";
 
 
-const Profile = () => {
+type ProfilePropsType ={
+    state:ProfilePageType
+    addPost: (postMessage: string) => void
+}
+
+
+
+const Profile: React.FC<ProfilePropsType> = (props) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPost/>
+            <MyPost addPost={props.addPost} post={props.state.post}/>
         </div>
 
     );
