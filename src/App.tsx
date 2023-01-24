bd1258893f5fa95e4e3a473bfbac0d86d69c487d
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-24 21:27:48 +0300

    Overturning data typing from BLL

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,30 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import {RootStatePropsType} from "./redux/state";
 
 
-const App = () => {
+type AppPropsType = {
+    state: RootStatePropsType
+    addPost:(postMessage: string) => void
+}
+
+
+const App: React.FC<AppPropsType>= (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
+
+
+
+
                     <Routes>
-                        <Route path='/dialogs' element={<Dialogs/>}/>
-                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path="/" element={<Profile state={props.state.profilePage} addPost={props.addPost}/>} />
+                        <Route path='/profile' element={<Profile state={props.state.profilePage} addPost={props.addPost} />}/>
+                        <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage}/>}/>
                         <Route path='/news' element={<News/>}/>
                         <Route path='/music' element={<Music/>}/>
                         <Route path='/settings' element={<Settings/>}/>
