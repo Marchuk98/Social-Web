0155568af0d863d5ca2d43419e80b8d052a9e3e5
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-01 00:35:52 +0300

    Return to the old rendering approach but with a revision that removes the cyclic dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
+import ReactDOM from "react-dom/client";
+import './index.css';
+import React from "react";
+import {addPost, updateNewPostText, state} from "./redux/state";
+import {subscribe} from "./redux/state";
+import App from "./App";
 
-import state,{addPost} from './redux/state'
-import {renderTree} from "./render";
 
 
-renderTree(state,addPost);
+
+export const renderEntireTree = () => {
+    const root = ReactDOM.createRoot(
+        document.getElementById('root') as HTMLElement
+    );
+    root.render(
+        <React.StrictMode>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>,
+    );
+}
 
 
+subscribe(renderEntireTree);
+renderEntireTree();
 
 
 
