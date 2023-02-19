9b8edce38791dd0fa113718a40ac928893c5637f
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-19 03:37:46 +0300

    State initialization and data typing for dialogs-reducer

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -1,11 +1,49 @@
-import {ActionTypeAC, DialogPageType, MessageType} from "./state";
-
-
 const SEND_MESSAGE = "SEND_MESSAGE";
 
 const updateMessageText = "updateMessageText"
 
- const dialogsReducer = (state: DialogPageType, action: ActionTypeAC) => {
+export type MessageType = {
+    id: number
+    message: string
+}
+
+export type DialogType = {
+    id: number
+    name: string
+}
+
+export type DialogPageType = {
+    dialogs: Array<DialogType>
+    message: Array<MessageType>
+    newMessageBody: string
+}
+
+
+let initialState: DialogPageType = {
+    dialogs: [
+        {id: 1, name: 'Vladimir'},
+        {id: 2, name: 'Ekaterina'},
+        {id: 3, name: 'Alexander'},
+        {id: 4, name: 'Victor'},
+        {id: 5, name: 'Victoria'},
+        {id: 6, name: 'Olga'},
+        {id: 7, name: 'Svetlana'}
+    ],
+    message: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are your'},
+        {id: 3, message: 'Normal'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'What'}
+
+    ],
+    newMessageBody: "Text"
+}
+
+
+type ActionTypeAC = SendMessageACType | UpdateNewMessageACType
+
+const dialogsReducer = (state: DialogPageType = initialState, action: ActionTypeAC) => {
     switch (action.type) {
         case "updateMessageText" :
             state.newMessageBody = action.newMessage;
@@ -23,22 +61,22 @@ const updateMessageText = "updateMessageText"
 };
 
 
+export type SendMessageACType = ReturnType<typeof SendMessageAC>
 
-    export type SendMessageACType = ReturnType<typeof SendMessageAC>
+export const SendMessageAC = (newMessageBody: string) => {
+    return {
+        type: SEND_MESSAGE,
+        newMessageBody: newMessageBody
+    } as const
+}
 
-    export const SendMessageAC = () => {
-        return {
-            type: SEND_MESSAGE,
-        } as const
-    }
+export type UpdateNewMessageACType = | ReturnType<typeof UpdateNewMessageAC>
 
-    export type UpdateNewMessageACType =  | ReturnType<typeof UpdateNewMessageAC>
+export const UpdateNewMessageAC = (newMessage: string) => {
+    return {
+        type: updateMessageText,
+        newMessage: newMessage
+    } as const
+}
 
-    export const UpdateNewMessageAC = (newMessage: string) => {
-        return {
-            type: updateMessageText,
-            newMessage: newMessage
-        } as const
-    }
-
-    export default dialogsReducer;
+export default dialogsReducer;
