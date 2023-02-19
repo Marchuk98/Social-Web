21c6199f8f056d6033c6a6dab7498fd68d5b22f6
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-19 03:38:33 +0300

    Refactoring components into a pure function

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,50 +1,38 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogsItems/DialogItem";
-import {ActionTypeAC, DialogPageType} from "../../redux/state";
 import MessageItem from "./MessageItem/MessageItem";
-import {SendMessageAC, UpdateNewMessageAC} from "../../redux/dialogs-reducer";
-
+import {DialogPageType} from "../../redux/dialogs-reducer";
 
 type DialogsPropsType = {
+    sendMessage: () => void
+    updateNewMessage: (newMessage: string) => void
     state: DialogPageType
-    dispatch: (action: ActionTypeAC) => void
 }
 
 
 const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
-    const DialogsElement = props.state.dialogs.map(d => <DialogItem key={d.id} id={d.id} name={d.name}/>)
-    const MessageElement = props.state.message.map(m => <MessageItem key={m.id} id={m.id} message={m.message}/>)
-
-
-    const NewDialogElement = React.createRef<HTMLTextAreaElement>();
-
-
     const onSendMessageClick = () => {
-        props.dispatch(SendMessageAC());
+        props.sendMessage();
 
     }
 
-    const onChangeTextAreaHandler = () => {
-
-        if (NewDialogElement.current) {
-            let text = NewDialogElement.current.value;
-            props.dispatch(UpdateNewMessageAC(text));
-        }
+    const onChangeTextAreaHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        props.updateNewMessage(e.currentTarget.value)
     }
-
     return (
         <div className={s.dialogs}>
             <div>
-                {DialogsElement}
+                {props.state.dialogs.map(d => <DialogItem key={d.id} id={d.id} name={d.name}/>)}
             </div>
             <div className={s.messages}>
-                <div>{MessageElement}</div>
                 <div>
-                    <div><textarea value={props.state.newMessageBody}
-                                   onChange={onChangeTextAreaHandler}
-                                   placeholder={'Enter your message'} ref={NewDialogElement}></textarea></div>
+                    {props.state.message.map(m => <MessageItem key={m.id} id={m.id} message={m.message}/>)}
+                </div>
+                <div>
+                    <div><textarea value={props.state.newMessageBody} onChange={onChangeTextAreaHandler}
+                                   placeholder={'Enter your message'}></textarea></div>
                     <div>
                         <button onClick={onSendMessageClick}>Send Message</button>
                     </div>
