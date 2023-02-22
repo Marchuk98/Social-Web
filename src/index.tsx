43231076df45492e8cb9df1d5d6843c7987f5976
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-22 23:44:29 +0300

    upd fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import './index.css';
 import React from "react";
 import store from './redux/redux-store'
 import App from "./App";
-import  {Provider} from "./StoreContext";
+import {Provider} from "react-redux";
+
 
 
 const root = ReactDOM.createRoot( document.getElementById('root') as HTMLElement)
 export const renderEntireTree = () => {
     root.render(
             <Provider store={store}>
-            <App /*store={store}*/ />
+            <App/>
             </Provider>
     );
 }
