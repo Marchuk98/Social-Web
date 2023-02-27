826ba1e139ec9aba91183f5d6f12f7d7817bba98
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-27 22:33:32 +0300

    Implementing a request to the server

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,45 +1,23 @@
 import React from 'react';
 import {usersStatePropsType} from "./UsersContainer";
 import s from './Users.module.css'
-
+import axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 const Users = (props:usersStatePropsType) => {
 
-    if(props.stateUserPage.users.length === 0){
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl:'https://media.istockphoto.com/vectors/cat-working-on-computer-vector-id165049167?k=20&m=165049167&s=612x612&w=0&h=XhBcoFA1oMU-KfUEMTMsN3WeBbHHxH08fS-vtDcpJOA=',
-                followed: false,
-                name: "Dima",
-                description: "I am a boss",
-                location: {city: "Minsk", country: "Belarus"}
-            },
-            {
-                id: 1,
-                photoUrl:'https://media.istockphoto.com/vectors/puppy-at-computer-vector-id165049259?k=20&m=165049259&s=612x612&w=0&h=kOu0Lrb5iez4u9Yfe1AXUDew9Nx0mHsgl_xpopP0-PM=',
-                followed: true,
-                name: "Vova",
-                description: "I am so fanny",
-                location: {city: "Brest", country: "Belarus"}
-            },
-            {
-                id: 1,
-                photoUrl:'https://damion.club/uploads/posts/2022-11/1668830902_damion-club-p-kotik-za-kompom-oboi-85.jpg',
-                followed: false,
-                name: "Sveta",
-                description: "I think she is so pretty.",
-                location: {city: "Moscow", country: "Russia"}
-            },
-        ])
+    if(props.stateUserPage.length === 0){
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+            props.setUsers(response.data.items)
+        });
     }
 
     return (
         <div>
-            {props.stateUserPage.users.map(el=> <div key={el.id}>
+            {props.stateUserPage.map(el=> <div key={el.id}>
                 <span>
                     <div>
-                        <img src={el.photoUrl} className={s.userPhoto}/>
+                        <img src={!el.photo ? userPhoto : el.photo} className={s.userPhoto} alt={'userPhoto'}/>
                     </div>
                     <div>
                        {el.followed
@@ -54,8 +32,8 @@ const Users = (props:usersStatePropsType) => {
                         <div>{el.description}</div>
                     </span>
                     <span>
-                         <div>{el.location.country}</div>
-                         <div>{el.location.city}</div>
+                         <div>{"el.location.country"}</div>
+                         <div>{"el.location.city"}</div>
                     </span>
                 </span>
             </div>)}
