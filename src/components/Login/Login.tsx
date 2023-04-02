2e321a02773409c587880b0cc9f691bd47fac30a
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-04-02 23:12:51 +0300

    Implementing redux-form to work with Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
-export const Login = () => {
+
+type LoginFormType = {
+    login:string
+    password:string
+    rememberMe:string
+}
+
+export const LoginForm:React.FC<InjectedFormProps<LoginFormType>> = (props) => {
     return (
         <div>
-            <h1>Login</h1>
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field  name={"login"} placeholder={"Login"} component={"input"} autoComplete="current-login"/>
+                </div>
+                <div>
+                    <Field  type={'password'} name={"Password"} placeholder={"Password"} component={"input"} autoComplete="current-password"/>
+                </div>
+                <div>
+                    <input type={'checkbox'}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
     );
 };
+
+const LoginReduxForm = reduxForm<LoginFormType>({form:'Login'})(LoginForm)
+
+
+export const Login = () => {
+
+    const onSubmitHandler = (formData:LoginFormType) => {
+        console.log(formData)
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmitHandler}/>
+        </div>
+    )
+}
 
